d1dd3fe6c6ea75ad39978f004ae0846bdaeb1aa9
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-08 14:51:46 -0500

    added fields to Skill class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String longDescription;
+
+    public Skill(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public Skill() {}
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
 }
