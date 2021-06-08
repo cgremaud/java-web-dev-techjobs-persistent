070ab7c9c8fb5e0dd4847f2fe1dbaf137b84531c
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-08 15:25:12 -0500

    answered part 2 SQL task

--- queries.sql
+++ queries.sql
@@ -6,6 +6,8 @@ name varchar
 skills varchar
 ## Part 2: Test it with SQL
 
+SELECT * FROM techjobs.employer WHERE (location = "St. Louis City");
+
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
