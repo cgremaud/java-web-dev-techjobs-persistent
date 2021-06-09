419c7c8ef7bead5c622ae01f44176e925f7314db
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-09 16:12:25 -0500

    Still need to finish sql task 4 but otherwise done.

--- queries.sql
+++ queries.sql
@@ -4,10 +4,14 @@ id int primary key
 employer varchar
 name varchar
 skills varchar
+
 ## Part 2: Test it with SQL
 
 SELECT * FROM techjobs.employer WHERE (location = "St. Louis City");
 
 ## Part 3: Test it with SQL
 
+DROP TABLE techjobs.job`;
+
 ## Part 4: Test it with SQL
+select name, description from skill where job_id = not null ##idk
