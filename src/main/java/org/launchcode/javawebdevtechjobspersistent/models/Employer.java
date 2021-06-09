4878223b6da7b7f3f924756b26975df90148b2a3
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-09 14:12:38 -0500

    Updated job and employer to have many to one rel

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(min = 3, max = 140)
     private String location;
+
+    @OneToMany
+    @JoinColumn//(name = "employer") //not sure if this needed yet
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(String location) {
         this.location = location;
